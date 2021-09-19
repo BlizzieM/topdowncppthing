6c2155ad3a82998af40a58aa4f53891295107d9b
BlizzieM <tuisku.penttila@outlook.com>
2021-09-19 19:33:17 +0300

    initial

---
+++ Maps/Final/grass biome.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="GrassBiome" tilewidth="16" tileheight="16" tilecount="320" columns="16">
+ <image source="../HAS Overworld 2.1/GrassBiome/GB-LandTileset.png" width="256" height="320"/>
+</tileset>
